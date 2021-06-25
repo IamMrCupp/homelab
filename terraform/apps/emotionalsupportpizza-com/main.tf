298a3198be97f7594d587bdf6a125bc7c8f1d8b4
Aaron Cupp <mrcupp@mrcupp.com>
2021-06-25 00:02:06 -0700

    add module to setup dns for ESP
    Signed-off-by: Aaron Cupp <mrcupp@mrcupp.com>

---
+++ terraform/apps/emotionalsupportpizza-com/main.tf
@@ -0,0 +1,9 @@
+/*
+  Emotional Support Pizza DNS
+  -  All the work is actually done in the module not this file
+*/
+
+# pull in the module for the DNS changes
+module "emotionalsupportpizza-namecheap-dns" {
+  source = "../../modules/dns-namecheap/emotionalsupportpizza-com/main.tf"
+}
