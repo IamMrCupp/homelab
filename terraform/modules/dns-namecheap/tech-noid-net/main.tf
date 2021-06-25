9cd980a385f7dd693f015d4f25a308f0a1e00d0a
Aaron Cupp <mrcupp@mrcupp.com>
2021-06-24 23:58:50 -0700

    add variables for the module
    Signed-off-by: Aaron Cupp <mrcupp@mrcupp.com>

--- terraform/modules/dns-namecheap/tech-noid-net/main.tf
+++ terraform/modules/dns-namecheap/tech-noid-net/main.tf
@@ -17,11 +17,11 @@ terraform {
 }
 
 provider "namecheap" {
-  username = "disfigure"          # Also set by env variable `NAMECHEAP_USERNAME`
-  api_user = "disfigure"          # Same as username; also set by env variable `NAMECHEAP_API_USER`
-  token = "your_token"            # Also set by env variable `NAMECHEAP_TOKEN`
-  ip = "your.ip.address.here"    # Also set by env variable `NAMECHEAP_IP`
-  use_sandbox = true              # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
+  username = var.username     # Also set by env variable `NAMECHEAP_USERNAME`
+  api_user = var.api_user     # Same as username; also set by env variable `NAMECHEAP_API_USER`
+  token = var.token           # Also set by env variable `NAMECHEAP_TOKEN`
+  ip = var.ip_address         # Also set by env variable `NAMECHEAP_IP`
+  use_sandbox = var.sandbox   # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
 }
 
 
