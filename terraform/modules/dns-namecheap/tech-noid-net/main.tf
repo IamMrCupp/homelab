923a8c9e2232c02b692c7eb7b286a075d7c9959b
Aaron Cupp <mrcupp@mrcupp.com>
2021-06-24 23:48:24 -0700

    update tech-noid.net dns module
    Signed-off-by: Aaron Cupp <mrcupp@mrcupp.com>

--- terraform/modules/dns-namecheap/tech-noid-net/main.tf
+++ terraform/modules/dns-namecheap/tech-noid-net/main.tf
@@ -1,7 +1,12 @@
-# file: dns/tech-noid.net/external/main.tf
-# - controls the domain names in DNS at namecheap.com
-# - domain: tech-noid.net
+/* 
+    file: modules/dns-namecheap/tech-noid-net/main.tf
+    - controls the domain names in DNS at namecheap.com
+    - domain: tech-noid.net
+*/
 
+/* 
+    Configure the providers and such that are in use in the module
+*/
 terraform {
   required_providers {
     namecheap = {
@@ -19,8 +24,64 @@ provider "namecheap" {
   use_sandbox = true              # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
 }
 
-# gmail CNAME for email
-resource "namecheap_record" "cname-mail-tech-noid-net" {
+
+/* 
+  *  DNS Entries 
+
+  ?  Use the following framework for the entries: 
+    resource "namecheap_record" "[TYPE]-[DESCRIPTIVE-NAME]-tech-noid" {
+        name = ""  
+        address = ""
+        type = ""
+        ttl = 60
+        domain = "tech-noid.net"
+    } 
+*/
+
+/*
+    A RECORDS
+*/
+
+/*  
+    AAAA RECORDS
+*/
+
+/*
+    ALIAS RECORDS
+*/
+
+/*
+    CAA RECORDS
+*/
+
+/*
+    CNAMES RECORDS
+*/
+resource "namecheap_record" "cname-calendar-tech-noid" {
+  name = "calendar"  
+  address = "ghs.google.com."
+  type = "CNAME"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+resource "namecheap_record" "cname-drive-tech-noid" {
+  name = "drive"  
+  address = "ghs.google.com."
+  type = "CNAME"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+resource "namecheap_record" "cname-group-tech-noid" {
+  name = "group"  
+  address = "ghs.google.com."
+  type = "CNAME"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+resource "namecheap_record" "cname-mail-tech-noid" {
   name = "mail"  
   address = "ghs.google.com."
   type = "CNAME"
@@ -28,12 +89,64 @@ resource "namecheap_record" "cname-mail-tech-noid-net" {
   domain = "tech-noid.net"
 } 
 
-# cname.bitly.com for tech-noid.net domain
-resource "namecheap_record" "cname-bitly-tech-noid-net" {
-  name = "226ceff29825"  
-  address = "cname.bitly.com."
+resource "namecheap_record" "cname-sites-tech-noid" {
+  name = "sites"  
+  address = "ghs.google.com."
   type = "CNAME"
   ttl = 60
   domain = "tech-noid.net"
 } 
 
+/*
+    MX RECORDS
+*/
+
+/*
+    MXE RECORDS
+*/
+
+/*
+    NS RECORDS
+*/
+
+/*
+    TXT RECORDS
+*/
+# google domain verification record
+resource "namecheap_record" "txt-google-domainverif-tech-noid" {
+  name = "@"  
+  address = ""
+  type = "TXT"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+# facebook domain verification record
+resource "namecheap_record" "txt-facebook-domainverif-tech-noid" {
+  name = "@"  
+  address = ""
+  type = "TXT"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+# domain keys for email at google
+resource "namecheap_record" "txt-google-domainkey-tech-noid" {
+  name = "google._domainkey"  
+  address = ""
+  type = "TXT"
+  ttl = 60
+  domain = "tech-noid.net"
+} 
+
+/*
+    URL RECORDS
+*/
+
+/*
+    URL301 RECORDS
+*/
+
+/*
+    FRAME RECORDS
+*/
