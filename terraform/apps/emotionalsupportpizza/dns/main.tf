c25ad425e7f8d841147408298603cd47b185dac8
Aaron Cupp <mrcupp@mrcupp.com>
2021-06-28 00:03:34 -0700

    more edits

--- terraform/apps/emotionalsupportpizza/dns/main.tf
+++ terraform/apps/emotionalsupportpizza/dns/main.tf
@@ -9,9 +9,9 @@
 module "emotionalsupportpizza-namecheap-dns" {
   source      = "../../../modules/dns-namecheap/emotionalsupportpizza/"
 
-  username    = var.username     # Also set by env variable `NAMECHEAP_USERNAME`
-  api_user    = var.api_user     # Same as username; also set by env variable `NAMECHEAP_API_USER`
-  token       = var.token        # Also set by env variable `NAMECHEAP_TOKEN`
-  ip_address  = var.ip_address   # Also set by env variable `NAMECHEAP_IP`
-  sandbox     = var.sandbox      # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
+  username    = "disfigure"       # Also set by env variable `NAMECHEAP_USERNAME`
+  api_user    = "disfigure"       # Same as username; also set by env variable `NAMECHEAP_API_USER`
+  token       = ""                # Also set by env variable `NAMECHEAP_TOKEN`
+  ip_address  = "var.ip_address"  # Also set by env variable `NAMECHEAP_IP`
+  sandbox     = true              # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
 }
--- terraform/modules/dns-namecheap/emotionalsupportpizza/main.tf
+++ terraform/modules/dns-namecheap/emotionalsupportpizza/main.tf
@@ -1,5 +1,6 @@
 /* 
     file: modules/dns-namecheap/emotionalsupportpizza.com/main.tf
+
     - controls the domain names in DNS at namecheap.com
     - domain: emotionalsupportpizza.com
 */
@@ -20,8 +21,8 @@ provider "namecheap" {
   username    = "${var.username}"     # Also set by env variable `NAMECHEAP_USERNAME`
   api_user    = "${var.api_user}"     # Same as username; also set by env variable `NAMECHEAP_API_USER`
   token       = "${var.token}"        # Also set by env variable `NAMECHEAP_TOKEN`
-  ip_address  = "${var.ip_address}"   # Also set by env variable `NAMECHEAP_IP`
-  sandbox     = "${var.sandbox}"      # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
+  ip          = "${var.ip_address}"   # Also set by env variable `NAMECHEAP_IP`
+  use_sandbox = "${var.sandbox}"      # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
 }
 
 
